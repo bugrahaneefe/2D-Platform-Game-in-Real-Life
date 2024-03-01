13778080e5ca50392d2f8138021c5c6bd5af3698
Buğrahan Efe <114180327+bugrahaneefe@users.noreply.github.com>
2024-03-01 11:44:23 +0300

    shooting basic are added.
    shooting basic are added.

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private float _speed = 10.0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementCalculation();
+    }
+
+    private void movementCalculation()
+    {
+            transform.Translate(Vector3.right * _speed * Time.deltaTime);
+
+        if (transform.position.x >= 9.257071f)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.x <= -9.245456f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,20 +4,52 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //private CharacterController _characterController;
     [SerializeField]
     private float _speed = 3.5f;
-    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject _bulletPrefab;
     void Start()
     {
+        //_characterController = GetComponent<CharacterController>();
         transform.position = new Vector3(0,-3.98f,0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        
-        transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
+        movementCalculation();
+        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+            Instantiate(_bulletPrefab, transform.position, Quaternion.identity);
+        }
+    }
 
+    private void movementCalculation()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+
+            transform.Translate(direction * _speed * Time.deltaTime);
+        }
+
+        if (transform.position.x >= 9.257071f)
+        {
+            transform.position = new Vector3(9.25707f, transform.position.y, 0);
+        }
+        if (transform.position.x <= -9.245456f)
+        {
+            transform.position = new Vector3(-9.24545f, transform.position.y, 0);
+        }
+        if (transform.position.y <= -3.98f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.98f, 0);
+        }
     }
 }
